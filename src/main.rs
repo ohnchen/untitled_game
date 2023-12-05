9293cc7edb260e0b54d2bfdab9d55c3b69197b18
Jona Ohme <ohmejona@gmail.com>
2023-12-05 22:53:04 +0100

    added map_regeneration

--- src/main.rs
+++ src/main.rs
@@ -32,13 +32,13 @@ fn main() -> io::Result<()> {
     let map_width: u16 = terminal::size()?.0;
     let map_height: u16 = terminal::size()?.1 - extra_height;
 
-    let map = Map::new(map_width, map_height);
+    let mut map = Map::new(map_width, map_height);
     map.draw_map()?;
 
     let mut player = Player::new(&map);
     execute!(
         io::stdout(),
-        MoveTo(map.width/2, map.height/2),
+        MoveTo(map.width / 2, map.height / 2),
         Print('X'),
         MoveLeft(1)
     )?;
@@ -46,24 +46,35 @@ fn main() -> io::Result<()> {
     loop {
         let old_player_pos: (u16, u16) = (player.x, player.y);
         //if event::poll(std::time::Duration::from_millis(500))? {
-            match event::read()? {
-                event::Event::Key(key_event) => match key_event.code {
-                    event::KeyCode::Esc => break,
-                    event::KeyCode::Char(c) => match c {
-                        'h' => player.move_direction(&map, Direction::Left, 1),
-                        'l' => player.move_direction(&map, Direction::Right, 1),
-                        'j' => player.move_direction(&map, Direction::Down, 1),
-                        'k' => player.move_direction(&map, Direction::Up, 1),
-                        'H' => player.move_direction(&map, Direction::Left, 3),
-                        'L' => player.move_direction(&map, Direction::Right, 3),
-                        'J' => player.move_direction(&map, Direction::Down, 3),
-                        'K' => player.move_direction(&map, Direction::Up, 3),
-                        _ => {}
-                    },
+        match event::read()? {
+            event::Event::Key(key_event) => match key_event.code {
+                event::KeyCode::Esc => break,
+                event::KeyCode::F(5) => {
+                    map = Map::new(map_width, map_height);
+                    map.draw_map()?;
+                    player = Player::new(&map);
+                    execute!(
+                        io::stdout(),
+                        MoveTo(map.width / 2, map.height / 2),
+                        Print('X'),
+                        MoveLeft(1)
+                    )?;
+                }
+                event::KeyCode::Char(c) => match c {
+                    'h' => player.move_direction(&map, Direction::Left, 1),
+                    'l' => player.move_direction(&map, Direction::Right, 1),
+                    'j' => player.move_direction(&map, Direction::Down, 1),
+                    'k' => player.move_direction(&map, Direction::Up, 1),
+                    'H' => player.move_direction(&map, Direction::Left, 3),
+                    'L' => player.move_direction(&map, Direction::Right, 3),
+                    'J' => player.move_direction(&map, Direction::Down, 3),
+                    'K' => player.move_direction(&map, Direction::Up, 3),
                     _ => {}
                 },
                 _ => {}
-            }
+            },
+            _ => {}
+        }
         //}
 
         map.draw_player(old_player_pos, &player)?;
