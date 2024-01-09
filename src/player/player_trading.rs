3f5b705a52490e1d24a48c2107ddf7d0acceceb5
Jona Ohme (rxcrh) <ohmejona@gmail.com>
2024-01-09 01:04:38 +0100

    trading isnt f***d anymore

--- src/player/player_trading.rs
+++ src/player/player_trading.rs
@@ -13,31 +13,16 @@ impl Player {
     }
 
     pub fn has_item(&mut self, item: &Item, count: i32) -> bool {
-        self.items[item] >= count
+        self.items[item] >= count.abs()
     }
 
     pub fn trade(&mut self, item: &Item, count: i32, merchant: &mut Merchant) -> Option<i32> {
         let cost = merchant.prices[item] as i32 * count;
-        let mut failed_trade = None;
-        if self.gold as i32 >= cost {
-            self.items.get_mut(item).map(|e| {
-                if count >= 0 || (count < 0 && count.abs() <= *e) {
-                    *e += count;
-                } else {
-                    failed_trade = Some(cost - self.gold as i32);
-                }
-            });
-            merchant.items.get_mut(item).map(|e| {
-                if count < 0 || (count > 0 && count.abs() <= *e) {
-                    *e -= count;
-                } else {
-                    failed_trade = Some(cost - self.gold as i32);
-                }
-            });
-
-            if failed_trade.is_some() {
-                return failed_trade;
-            }
+        if (cost < 0 && self.has_item(item, count))
+            || (self.has_money(cost) && cost > 0 && merchant.has_item(item, count))
+        {
+            self.items.get_mut(item).map(|e| *e += count);
+            merchant.items.get_mut(item).map(|e| *e -= count);
 
             self.gold = (self.gold as i32 - cost) as u32;
             return None;
